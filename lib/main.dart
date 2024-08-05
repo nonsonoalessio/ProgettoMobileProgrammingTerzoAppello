112f841838f640860fe50c822e5ac800fbd1b6bd
Alessio Donato Giura <alessio.giura@gmail.com>
2024-08-05 14:59:35 +0200

    Aggiunta navigationBar.

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:progetto_mobile_programming/views/homepage.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Homepage(),
+      home: Scaffold(
+        body: Navigation(),
+      ),
+    );
+  }
+}
+
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  int currentPageIndex = 0;
+  final List<Widget> _destinations = [
+    const NavigationDestination(
+      icon: Icon(Icons.home),
+      label: 'Home',
+    ),
+    const NavigationDestination(
+      icon: Icon(Icons.person),
+      label: 'Profile',
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: <Widget>[
+        const Homepage(),
+        const Scaffold(
+          body: Center(
+            child: Text('Hello World!'),
+          ),
+        ),
+      ][currentPageIndex],
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        destinations: _destinations,
+      ),
     );
   }
 }
