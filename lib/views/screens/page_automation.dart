7ad43c15c01411103d98f93a70109bcdf8a093a7
Mario D'Aniello <115697716+mariodan02@users.noreply.github.com>
2024-08-14 16:59:56 +0200

    Page automation ora sfrutta i provider

--- lib/views/screens/page_automation.dart
+++ lib/views/screens/page_automation.dart
@@ -1,47 +1,42 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:progetto_mobile_programming/providers/automations_provider.dart';
 import 'package:progetto_mobile_programming/views/screens/add_new_items_screens/add_new_automation.dart';
 
-class AutomationPage extends StatelessWidget {
+class AutomationPage extends ConsumerWidget {
   const AutomationPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final automations = ref.watch(automationsNotifierProvider);
+
     return Scaffold(
-      
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Header
-              Text(
-                'Nome Automazione',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+            child: automations.isEmpty
+              ? Center(child: Text('Non hai aggiunto nessuna automazione')) // Caso in cui la lista è vuota
+               : ListView.builder(
+                itemCount: automations.length,
+                itemBuilder: (context, index){
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ListTile(
+                          title: Text(automations[index].name),
+                          subtitle: Text("L'automazione andrà in azione alle ore ${automations[index].executionTime}"),
+                          onTap: () {
+                            // Porta a pagina di dettaglio
+                          },
+                        ),
+                        Divider(),
+                      ],
+                    );
+                  },
                 ),
-              ),
-              SizedBox(height: 4),
-              Text(
-                'Descrizione di cosa fa l\'automazione',
-                style: TextStyle(
-                  fontSize: 14,
-                ),
-              ),
-              SizedBox(height: 8),
-              Text(
-                'Quando: condizione scatenante',
-                style: TextStyle(
-                  fontSize: 14,
-                ),
-              ),
-              Divider(),
-              // Spazio vuoto per aggiungere altri elementi
-            ],
-          ),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
