aa1a85cb7c02a83f6a7a5172a14f2679d8f438a3
simoneGuerra0 <148072510+simoneGuerra0@users.noreply.github.com>
2024-08-29 10:46:22 +0200

    notifiche

--- lib/views/screens/page_all_notification.dart
+++ lib/views/screens/page_all_notification.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:progetto_mobile_programming/models/functionalities/device_notification.dart';
+import 'package:progetto_mobile_programming/models/objects/device.dart';
 import 'package:progetto_mobile_programming/providers/notifications_provider.dart';
+import 'package:progetto_mobile_programming/views/screens/page_device_detail.dart';
 
 class NotificationPage extends ConsumerStatefulWidget {
   const NotificationPage({super.key});
@@ -12,25 +14,19 @@ class NotificationPage extends ConsumerStatefulWidget {
 }
 
 class _NotificationPageState extends ConsumerState<NotificationPage> {
-  /*
-  final List<DeviceNotification> _notifications = [
-    DeviceNotification(
-      title: "Titolo 1",
-      description: "Testo notifica 1",
-      device: Light(deviceName: 'Luce di emergenza', room: '1',
-      id: 
-      ),
-      type: NotificationType.security,
-      deliveryTime: DateTime.now().millisecondsSinceEpoch,
-      isRead: false,
-    ),
-  ];
-*/
-
   void _markAsRead(DeviceNotification notification) {
     setState(() {
       notification.isRead = true;
     });
+  }
+
+  void _navigateToDeviceDetail(Device device) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DeviceDetailPage(device: device),
+      ),
+    );
   }
 
   @override
@@ -43,29 +39,35 @@ class _NotificationPageState extends ConsumerState<NotificationPage> {
       appBar: AppBar(
         title: const Text('Notifiche'),
         leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.arrow_back_ios_new)),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios_new),
+        ),
       ),
       body: SafeArea(
         child: ListView.builder(
           itemCount: notificationsList.length,
-          itemBuilder: (context, index) => GestureDetector(
-            onTap: () {
-              // Quando una notifica non letta viene cliccata, la segniamo come letta
-              if (!notificationsList[index].isRead) {
-                _markAsRead(notificationsList[index]);
-              }
-              // Naviga pagina dettagli
-            },
-            child: NotificationCard(notification: notificationsList[index]),
-          ),
+          itemBuilder: (context, index) {
+            final notification = notificationsList[index];
+            return GestureDetector(
+              onTap: () {
+                // Quando una notifica non letta viene cliccata, la segniamo come letta
+                if (!notification.isRead) {
+                  _markAsRead(notification);
+                }
+                // Naviga alla pagina di dettaglio del dispositivo associato
+                _navigateToDeviceDetail(notification.device);
+              },
+              child: NotificationCard(notification: notification),
+            );
+          },
         ),
       ),
     );
   }
 }
+
 
 class NotificationCard extends StatelessWidget {
   final DeviceNotification notification;
@@ -86,8 +88,7 @@ class NotificationCard extends StatelessWidget {
             Icons.notifications,
             color: notification.isRead
                 ? Colors.grey
-                : Colors
-                    .green, // Mostra icona verde per le notifiche non lette e icona grigia per le notifiche già lette
+                : Colors.green, // Mostra icona verde per le notifiche non lette e icona grigia per le notifiche già lette
           ),
           const SizedBox(width: 16.0),
           Expanded(
@@ -133,3 +134,4 @@ class NotificationCard extends StatelessWidget {
     );
   }
 }
+
