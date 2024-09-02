e0b6cc0ad04e1fca37baaf7d90dca8b05298962b
Mario D'Aniello <115697716+mariodan02@users.noreply.github.com>
2024-09-02 15:50:57 +0200

    Update database_helper.dart

--- lib/services/database_helper.dart
+++ lib/services/database_helper.dart
@@ -521,7 +521,12 @@ class DatabaseHelper {
     final db = await database;
     final String deliveryTimeString =
         '${notification.deliveryTime.hour.toString().padLeft(2, '0')}:${notification.deliveryTime.minute.toString().padLeft(2, '0')}';
-    
+    final int isReadInt;
+    if (notification.isRead) {
+      isReadInt = 1;
+    } else {
+      isReadInt = 0;
+    }
     await db.insert(
       'deviceNotification',
       {
@@ -529,7 +534,7 @@ class DatabaseHelper {
         'title': notification.title,
         'device': notification.device.id,
         'deliveryTime': deliveryTimeString,
-        'isRead': notification.isRead,
+        'isRead': isReadInt,
         'description': notification.description,
       },
     );
